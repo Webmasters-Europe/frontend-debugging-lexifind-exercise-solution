17bac558bf27bb03c34adab0ba3b7b5712673535
kaumonaung <47107420+kaumonaung@users.noreply.github.com>
2024-07-24 17:49:46 +0200

    Finished LexiFind exercise solution

---
+++ src/components/FavoriteWord.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { HeartFilled, HeartIcon } from './Icons';
+
+interface FavoriteWordProps {
+  word: string;
+}
+
+const FavoriteWord: React.FC<FavoriteWordProps> = ({ word }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    const favorites = JSON.parse(localStorage.getItem('favoriteWords') || '[]');
+
+    if (favorites.includes(word)) {
+      const filteredFavorites = favorites.filter(
+        (item: string) => item !== word
+      );
+      localStorage.setItem('favoriteWords', JSON.stringify(filteredFavorites));
+    } else {
+      favorites.push(word);
+      localStorage.setItem('favoriteWords', JSON.stringify(favorites));
+    }
+    setIsFavorite(!isFavorite);
+  };
+
+  useEffect(() => {
+    const favoriteWords = JSON.parse(
+      localStorage.getItem('favoriteWords') || '[]'
+    );
+    setIsFavorite(favoriteWords.includes(word));
+  }, [word]);
+
+  return (
+    <div className="fave-word-wrapper">
+      <Link to={`/?word=${word}`} className="fave-word-link">
+        <p>{word}</p>
+      </Link>
+
+      <button className="fave-icon-btn" onClick={toggleFavorite}>
+        {isFavorite ? (
+          <HeartFilled className="heart-icon-filled" aria-hidden="true" />
+        ) : (
+          <HeartIcon className="heart-icon-unfilled" aria-hidden="true" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default FavoriteWord;
